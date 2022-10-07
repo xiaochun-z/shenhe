522d28c5f39760f853910322fb307a434680302a
Caden <madcaden@outlook.com>
2022-10-07 09:11:19 +0800

    update the size of the page

--- src/Pages/TwentyFour.tsx
+++ src/Pages/TwentyFour.tsx
@@ -85,20 +85,22 @@ export const TwentyFour: FC = () => {
         }
     };
     return (
-        <div className='tfroot container mt-5'>
+        <div className='container mt-1'>
             <div className="card">
-                <img src={logo} className="card-img-top card-24-logo" alt="..." />
+                <img src={logo} className="card-img-top" alt="..." />{/*max size: 1294x450*/}
                 <div className="card-body">
                     <h5 className="card-title">24 点</h5>
                     <p className="card-text">24点游戏（在香港也称“合廿四”）是一种使用扑克牌来进行的益智类游戏，游戏内容是：从一副扑克牌中抽去大小王，在剩下52张中任意抽取4张牌，把牌面上的数（A代表1）运用加、减、乘、除和括号进行运算得出24。每张牌都必须使用一次，但不能重复使用。在不同版本中，对J、Q、和K的处理有些差异。一个常见的版本是把J、Q、和K去除，或当成10；还有一个版本是把J表示11，Q表示12，K代表13。</p>
                     <p>有些组合有多种算法，例如2，4，6，Q四张牌可用 2 + 4 + 6 + 12 = 24 或 4×6 ÷ 2 + 12 = 24 或 12 ÷ 4×(6 + 2) = 24等来求解。也有些组合算不出24，如1、1、1、1 和 6、7、8、8等组合。</p>
                     <p>这个小程序可以作为24点游戏的辅助，帮助你快速了解你的牌能否达成24点。</p>
                     <label htmlFor="tfvalue" className="form-label">请输入4个数字（以空格分隔，例如：<span className="text-pink">2 4 5 10</span>），如果你输入的不是数字，不是数字的部分会被自动忽略。</label>
-                    <input type="text" className="form-control mb-2" id="tfvalue" placeholder="2 4 5 10" value={strcards} onChange={onTexFieldChange} onKeyDown={onTextFieldKeydown} />
-                    <button className='me-2 btn btn-sm btn-primary' onClick={verifycards}>查看是否能够凑成24</button>
-                    <button className='me-2 btn btn-sm btn-info' onClick={showTips}>查看提示</button>
-                    <button className='me-2 btn btn-sm btn-success' onClick={showAnswer}>查看答案</button>
-                    <button className='btn btn-sm btn-secondary' onClick={clearAnswer}>清空答案</button>
+                    <input type="text" name="tf_zip" className="form-control mb-3" id="tfvalue" placeholder="2 4 5 10" value={strcards} onChange={onTexFieldChange} onKeyDown={onTextFieldKeydown} />
+                    <div>
+                        <button className='me-2 mt-1 btn btn-sm btn-primary' onClick={verifycards}>查看是否能够凑成24</button>
+                        <button className='me-2 mt-1 btn btn-sm btn-info' onClick={showTips}>查看提示</button>
+                        <button className='me-2 mt-1 btn btn-sm btn-success' onClick={showAnswer}>查看答案</button>
+                        <button className='me-2 btn mt-1 btn-sm btn-secondary' onClick={clearAnswer}>清空答案</button>
+                    </div>
                     <div className='answer'>
                         <AnswerComponent Answers={answer} />
                     </div>
